f6a0e10e08128ba2dfe884495c8d95a6f22c7cb8
Lishen99 <lishen.madusha@gmail.com>
2025-06-28 18:22:23 -0400

    Add basePath and assetPrefix to next.config for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: '/Lishen-Amaraweera-Resume',
+  assetPrefix: '/Lishen-Amaraweera-Resume/',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
