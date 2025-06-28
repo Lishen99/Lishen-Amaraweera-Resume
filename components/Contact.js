9ff0f13970a4f9ece1e209fdba9a4a9f49090289
Lishen99 <lishen.madusha@gmail.com>
2025-06-28 19:36:45 -0400

    Update Contact component to use a constant base path for social media icons

--- components/Contact.js
+++ components/Contact.js
@@ -2,6 +2,8 @@
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 
+const GITHUB_PAGES_BASE_PATH = '/Lishen-Amaraweera-Resume';
+
 const Contact = () => (
     <motion.section
         id="contact"
@@ -13,9 +15,9 @@ const Contact = () => (
         <h2>Contact Me</h2>
         <p>Email: lishen.madusha@gmail.com</p>
         <p className="flex justify-center items-center">
-            <a className="links px-[0.5%]" href="https://www.instagram.com/lishen_99/" target="_blank"><Image className="zoom" src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/icons/instagram.png`} width={30} height={30} alt="Instagram"/></a>
-            <a className="links px-[0.5%]" href="https://github.com/Lishen99" target="_blank"><Image className="zoom" src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/icons/github.png`} width={30} height={30} alt="GitHub"/></a>
-            <a className="links px-[0.5%]" href="https://www.linkedin.com/in/lishen-amaraweera-88507a291/" target="_blank"><Image className="zoom" src={`${process.env.NEXT_PUBLIC_BASE_PATH || ''}/icons/linkedin.png`} width={30} height={30} alt="LinkedIn"/></a>
+            <a className="links px-[0.5%]" href="https://www.instagram.com/lishen_99/" target="_blank"><Image className="zoom" src={`${GITHUB_PAGES_BASE_PATH}/icons/instagram.png`} width={30} height={30} alt="Instagram"/></a>
+            <a className="links px-[0.5%]" href="https://github.com/Lishen99" target="_blank"><Image className="zoom" src={`${GITHUB_PAGES_BASE_PATH}/icons/github.png`} width={30} height={30} alt="GitHub"/></a>
+            <a className="links px-[0.5%]" href="https://www.linkedin.com/in/lishen-amaraweera-88507a291/" target="_blank"><Image className="zoom" src={`${GITHUB_PAGES_BASE_PATH}/icons/linkedin.png`} width={30} height={30} alt="LinkedIn"/></a>
         </p>
     </motion.section>
 );
