a6c1a953bfde854d4ec872b2f0f8d05924325feb
Lishen Amaraweera <84113512+Lishen99@users.noreply.github.com>
2025-06-28 19:40:38 -0400

    Update Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => (
         <p>Email: lishen.madusha@gmail.com</p>
         <p className="flex justify-center items-center">
             <a className="links px-[0.5%]" href="https://www.instagram.com/lishen_99/" target="_blank"><Image className="zoom" src={`${GITHUB_PAGES_BASE_PATH}/icons/instagram.png`} width={30} height={30} alt="Instagram"/></a>
-            <a className="links px-[0.5%]" href="https://github.com/Lishen99" target="_blank"><Image className="zoom" src={`${GITHUB_PAGES_BASE_PATH}/icons/github.png`} width={30} height={30} alt="GitHub"/></a>
+            <a className="links px-[0.5%]" href="https://github.com/Lishen99" target="_blank"><Image className="zoom" src={`Lishen-Amaraweera-Resume/icons/github.png`} width={30} height={30} alt="GitHub"/></a>
             <a className="links px-[0.5%]" href="https://www.linkedin.com/in/lishen-amaraweera-88507a291/" target="_blank"><Image className="zoom" src={`${GITHUB_PAGES_BASE_PATH}/icons/linkedin.png`} width={30} height={30} alt="LinkedIn"/></a>
         </p>
     </motion.section>
